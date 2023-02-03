13314fc0164980c1c2751b1058ce8594b2947220
Paco <76184617+PacoSLO@users.noreply.github.com>
2023-02-03 14:45:16 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from discord.ext import commands
+from discord.ext import tasks, commands
 import discord
+import asyncio
+from discord.ext.commands import Bot
+from discord.ext.commands import Context
 
 # Just add your desired prefix there.
 bot = commands.Bot(command_prefix='!')
@@ -13,15 +16,14 @@ async def on_ready():  # This function is run upon the bots startup completing
     print("Status: Running")
     print("########################################")
 
+#REFREST STATUS MSG
+@tasks.loop(seconds=18000)
+async def status_task() -> None:
     members = 0
     for guild in bot.guilds:
-        members += guild.member_count - 0    # I've added a '-1' because guild.member_count includes all users and bots including your own bot
-    
-    await bot.change_presence(activity = discord.Activity(
-        type = discord.ActivityType.watching,
-        name = f'{members} Members'
-    ))
+        members += guild.member_count - 0
+    await bot.change_presence(activity = discord.Activity(type = discord.ActivityType.watching, name = f'{members} Members'))
+    await asyncio.sleep(18000)
 
 #BOT TOKEN
 bot.run('YOUR TOKEN HERE')
-
